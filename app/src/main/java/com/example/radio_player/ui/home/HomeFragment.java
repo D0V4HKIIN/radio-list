aab506d72e85c14eb5e178a13c2bc6c5a2ff2613
jonas <jonas.bonnaudet@gmail.com>
2023-07-22 22:59:16 +0200

    radios now can play

--- app/src/main/java/com/example/radio_player/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/radio_player/ui/home/HomeFragment.java
@@ -1,9 +1,10 @@
 package com.example.radio_player.ui.home;
 
 import android.annotation.SuppressLint;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,24 +20,55 @@ import com.example.radio_player.RadioData;
 import com.example.radio_player.databinding.FragmentHomeBinding;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
+    private MediaPlayer mediaPlayer;
+    private Boolean mediaPlayerIsSet = false;
+    private RadioGroup radioGroup;
+    private RadioData rdata;
 
     @SuppressLint("SetTextI18n")
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 //        HomeViewModel homeViewModel =
 //                new ViewModelProvider(this).get(HomeViewModel.class);
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        radioGroup = root.findViewById(R.id.radioGroup);
 
-        download_radios();
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+
+        show_radios();
+
+        radioGroup.setOnCheckedChangeListener((radioGroup, i) -> {
+            View radioButton = radioGroup.findViewById(i);
+            int index = radioGroup.indexOfChild(radioButton);
+
+            try {
+                if(mediaPlayerIsSet){
+                    mediaPlayer.stop();
+                    mediaPlayer.release();
+                    mediaPlayer = new MediaPlayer();
+                    mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                }
+
+                mediaPlayerIsSet = true;
+
+                mediaPlayer.setDataSource(rdata.getRadio_list().get(index).get(1));
+                mediaPlayer.prepareAsync();
+                mediaPlayer.setOnPreparedListener(MediaPlayer::start);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
 
 
 //        final TextView textView = binding.textHome;
@@ -44,30 +76,26 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    private void download_radios() {
+    private void show_radios() {
         final ObjectMapper mapper = new ObjectMapper();
         final Handler handler = new Handler();
         new Thread(() -> {
             try {
-                Log.d("dl", "requesting...");
-                RadioData rdata = mapper.readValue(new URL("http://4jmb.free.fr/radio/radio.json"), RadioData.class);                                 // read from url
-                handler.post(() ->
-                        {
-                            RadioGroup radioGroup = this.binding.getRoot().findViewById(R.id.radioGroup);
+                rdata = mapper.readValue(new URL("http://4jmb.free.fr/radio/radio.json"), RadioData.class);                                 // read from url
+                handler.post(() -> {
 
-                            List<List<String>> radio_list = rdata.getRadio_list();
+                    List<List<String>> radio_list = rdata.getRadio_list();
 
-                            for (int i = 0; i < radio_list.size(); i++) {
-                                RadioButton radioButton = new RadioButton(getActivity());
-                                radioButton.setText(radio_list.get(i).get(0));
+                    for (int i = 0; i < radio_list.size(); i++) {
+                        RadioButton radioButton = new RadioButton(getActivity());
+                        radioButton.setText(radio_list.get(i).get(0));
 
-                                LinearLayout.LayoutParams layout_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+                        LinearLayout.LayoutParams layout_params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
 
-                                radioButton.setLayoutParams(layout_params);
-                                radioGroup.addView(radioButton);
-                            }
-                        }
-                );
+                        radioButton.setLayoutParams(layout_params);
+                        radioGroup.addView(radioButton);
+                    }
+                });
             } catch (Exception e) {
                 e.printStackTrace();
             }
